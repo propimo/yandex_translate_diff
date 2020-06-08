48f74415ab1420d7b80c8a4c856d585e9c91d162
nika-del <kimveronika25@gmail.com>
2020-06-08 21:48:30 +0400

    Added tests for optimized translation

--- spec/spec/yandex_translator_spec.rb
+++ spec/spec/yandex_translator_spec.rb
@@ -5,26 +5,48 @@ describe Translator::YandexTranslator do
 
   before { @translator = Translator::YandexTranslator.new }
 
-  #add FORMAT HTML
+  context "plain" do
+    it "should translate correctly plain sentence ru-en" do
+      expect(@translator.translate("Тестирование библиотеки."))
+          .to eql("Testing of the library. ")
+    end
 
-  it "should translate correctly plain sentence ru-en" do
-    expect(@translator.translate("Тестирование библиотеки."))
-        .to eql("Testing of the library. ")
+    it "should translate correctly plain sentence en-ru" do
+      expect(@translator.translate("Testing of the library.", from: 'en', to: 'ru'))
+          .to eql("Тестирование библиотеки. ")
+    end
+
+    it "should translate correctly several plain sentences ru-en" do
+      expect(@translator.translate("Тестирование библиотеки. Второй тест на русско - английский перевод.", from: 'ru', to: 'en'))
+          .to eql("Testing of the library. The second test for Russian - English translation. ")
+    end
+
+    it "should translate correctly several plain sentences en-ru" do
+      expect(@translator.translate("Testing of the library. Second test for english - russian translation.", from: 'en', to: 'ru'))
+          .to eql("Тестирование библиотеки. Второй тест на англо - русский перевод. ")
+    end
   end
 
-  it "should translate correctly plain sentence en-ru" do
-    expect(@translator.translate("Testing of the library.", from: 'en', to: 'ru'))
-        .to eql("Тестирование библиотеки. ")
-  end
+  context "html" do
+    it "should translate correctly plain sentence ru-en" do
+      expect(@translator.translate("<b>Тестирование библиотеки.</b>", format: "html"))
+          .to eql("<b>Testing of the library.</b> ")
+    end
 
-  it "should translate correctly several plain sentences ru-en" do
-    expect(@translator.translate("Тестирование библиотеки. Второй тест на русско - английский перевод.", from: 'ru', to: 'en'))
-        .to eql("Testing of the library. The second test for Russian - English translation. ")
-  end
+    it "should translate correctly plain sentence en-ru" do
+      expect(@translator.translate("<i>Testing of the library.</i>", from: 'en', to: 'ru', format: "html"))
+          .to eql("<i>Тестирование библиотеки.</i> ")
+    end
 
-  it "should translate correctly several plain sentences en-ru" do
-    expect(@translator.translate("Testing of the library. Second test for english - russian translation.", from: 'en', to: 'ru'))
-        .to eql("Тестирование библиотеки. Второй тест на англо - русский перевод. ")
+    it "should translate correctly several plain sentences ru-en" do
+      expect(@translator.translate("Тестирование <b>библиотеки</b>. Второй тест на русско - английский перевод.", from: 'ru', to: 'en', format: "html"))
+          .to eql("Testing <b>of the library</b>. The second test for Russian - English translation. ")
+    end
+
+    it "should translate correctly several plain sentences en-ru" do
+      expect(@translator.translate("Testing of the library. <b><i>Second test for english - russian translation.</i></b>", from: 'en', to: 'ru', format: "html"))
+          .to eql("Тестирование библиотеки. <b><i>Второй тест на англо - русский перевод.</i></b> ")
+    end
   end
 
   describe :delete_sentence do
@@ -168,13 +190,73 @@ describe Translator::YandexTranslator do
       end
   end
 
-=begin
   describe :update_translation do
-    it "Should translate en-ru whole updated original" do
-      expect(@translator.update_translation("", "" ,"" ))
-          .to eql("")
+
+    context "plain" do
+
+    it "Should translate ru-en whole updated original" do
+      expect(@translator.update_translation("", "Квартира с видом на море. 2 комнаты с хорошим ремонтом." ,"" ))
+          .to eql("Apartment with sea views. 2 rooms in good repair. ")
+    end
+
+    it "Should translate ru-en only last sentence" do
+      expect(@translator.update_translation("Квартира с видом на море.", "Квартира с видом на море. 2 комнаты с хорошим ремонтом." ,"Apartment with sea views." ))
+          .to eql("Apartment with sea views. 2 rooms in good repair.")
+    end
+
+
+    it "Should translate ru-en and replace last sentence" do
+      expect(@translator.update_translation("Квартира с видом на море. 2 комнаты с хорошим ремонтом.", "Квартира с видом на море. 2 комнаты с плохим ремонтом." ,"Apartment with sea views. 2 rooms in good repair." ))
+          .to eql("Apartment with sea views. 2 rooms with a bad repair.")
+    end
+
+    it "Should translate ru-en and replace middle sentence" do
+      expect(@translator.update_translation("Квартира с видом на море. 2 комнаты с хорошим ремонтом. Бассейн во дворе.", "Квартира с видом на море. 2 комнаты с отличным ремонтом. Бассейн во дворе." ,"Apartment with sea views. 2 rooms in good repair. The pool in the yard." ))
+          .to eql("Apartment with sea views. 2 rooms with excellent repair. The pool in the yard.")
+    end
+
+    it "Shouldn't translate anything" do
+      expect(@translator.update_translation("Квартира с видом на море. 2 комнаты с хорошим ремонтом. Бассейн во дворе.", "Квартира с видом на море. 2 комнаты с хорошим ремонтом. Бассейн во дворе." ,"First translation. Second translation. Last translation." ))
+          .to eql("First translation. Second translation. Last translation." )
+    end
+
+    it "Should translate and remove" do
+      expect(@translator.update_translation("Квартира с видом на море. 2 комнаты с хорошим ремонтом. Бассейн во дворе.", "Дом в лесу. 4 комнаты и сан-узел." ,"Apartment with sea views. 2 rooms in good repair. The pool in the yard." ))
+          .to eql("House in the woods. 4 rooms and WC.")
+    end
+
+    it "Should translate sentence without '.'" do
+      expect(@translator.update_translation("Квартира с видом на море. 2 комнаты с хорошим ремонтом. Бассейн во дворе.", "Дом" ,"Apartment with sea views. 2 rooms in good repair. The pool in the yard." ))
+          .to eql("House")
+    end
+
+    end
+
+    context "html" do
+
+      it "Should translate and keep boldness " do
+        expect(@translator.update_translation("<p><b> Квартира с видом на море. </b>2 комнаты с хорошим ремонтом. Бассейн во дворе. </p>", "<p><b>Дом с видом на море. </b>2 комнаты с хорошим ремонтом. Бассейн во дворе.</p>" ,
+                                              "<p><b>Apartment with sea views. </b>2 rooms in good repair. The pool in the yard. </p>", format: "html"))
+            .to eql("<p><b>House with sea views. </b>2 rooms in good repair. The pool in the yard. </p>")
+      end
+
+      it "Should translate and add boldness " do
+        expect(@translator.update_translation("<p>Квартира с видом на море. 2 комнаты с хорошим ремонтом. Бассейн во дворе.</p>", "<p><b>Дом с видом на море. </b>2 комнаты с хорошим ремонтом. Бассейн во дворе.</p>" ,
+                                              "<p>Apartment with sea views. 2 rooms in good repair. The pool in the yard.</p>", format: "html" ))
+            .to eql("<p><b>House with sea views. </b>2 rooms in good repair. The pool in the yard.</p>")
+      end
+
+      it "Should translate, keep italics and add boldness " do
+        expect(@translator.update_translation("<i>Квартира с видом на море. 2 комнаты с хорошим ремонтом.</i> Бассейн во дворе.", "<i>Квартира с видом на море. <b>2 комнаты.</b></i> Бассейн во дворе." ,
+                                              "<i>Apartment with sea views. 2 rooms in good repair.</i> The pool in the yard.", format: "html"))
+            .to eql("<i>Apartment with sea views. <b>2 rooms. </b></i> The pool in the yard.")
+      end
+
+      it "Should translate and replace &nbsp! " do
+        expect(@translator.update_translation("<i>Квартира&nbsp;с&nbsp;видом на море. 2&nbsp;комнаты с хорошим ремонтом.</i> Бассейн во дворе.", "<i>Квартира&nbsp;с&nbsp;видом на океан. <b>2&nbsp;комнаты.</b></i> Бассейн во дворе." ,
+                                              "<i>Apartment with sea views. 2 rooms in good repair.</i> The pool in the yard.", format: "html" ))
+            .to eql("<i>Apartment with ocean views. <b>2 rooms. </b></i> The pool in the yard.")
+      end
     end
   end
-=end
-
 end
